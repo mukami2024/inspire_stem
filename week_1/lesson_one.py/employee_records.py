890f25099e7a8db26a1830159830368101cd0742
mukami2024 <faithmukamimwangi@gmail.com>
2024-02-22 08:48:01 +0300

    employee_records

---
+++ week_1/lesson_one.py/employee_records.py
@@ -0,0 +1,32 @@
+#this is a program to show employee dat
+# date:21/02/2024
+#name: Faith Mukami
+
+#employee name : Richard Wesa
+#age : 24
+#salary : 250000
+#bonus of the employee:50000
+
+# increase salary by 30%
+salary= int(250000)
+new_salary= salary * (salary * 30/100)
+
+# decrease bonus by 5000
+bonus=int(50000)
+new_bonus=bonus - int(5000)
+
+print("the new salary is :",new_salary)
+print("the new bonus is :", new_bonus)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week_1/lesson_one.py/variables.py
@@ -0,0 +1,38 @@
+#data types in python 
+#date:
+#name:
+
+#integer are positive/negative whole numbers
+
+x=10
+y= -24
+
+#float: decimal/floating point
+pi = 3.142
+
+#characters : alphanumeric , 0-9 , A-Z
+#special characters : ! # $ @ * ,
+
+grade= 'A'
+
+# strings : a group of characters
+
+f_name = 'Faith' # use single quotes
+s_name= "Mukami" # use double quotes
+
+#naming conventions
+#camel case nameOfCity --> first letter in the first word is lower case , the rest of the words
+#snake case  name_of_city --> all are lower case
+
+height=20*2
+#print (height) -->40
+
+# constants -->dont change
+
+pi= 3.142
+
+# Avogadros constant
+avogadrosConstant = 6.029 * (10 **23)
+
+#plancks constant
+h = 6.62607015 * (10 ** (-34))
